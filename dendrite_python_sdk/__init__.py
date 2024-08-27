5edac1f8c70d2a572b7f36e0d669496be3d46e95
Arian Hanifi <counterstrike1337@hotmail.com>
2024-08-27 14:49:48 -0700

    major refactor of sdk and polishing

--- dendrite_python_sdk/__init__.py
+++ dendrite_python_sdk/__init__.py
@@ -1,2 +1,13 @@
-from .dendrite_browser.DendriteBrowser import DendriteBrowser
-from .dendrite_browser.DendriteRemoteBrowser import DendriteRemoteBrowser
+from ._core.dendrite_browser import DendriteBrowser
+from ._core.dendrite_remote_browser import DendriteRemoteBrowser
+from ._core.dendrite_element import DendriteElement
+from ._core.dendrite_page import DendritePage
+from ._core.models.response import DendriteElementsResponse
+
+__all__ = [
+    "DendriteBrowser",
+    "DendriteRemoteBrowser",
+    "DendriteElement",
+    "DendritePage",
+    "DendriteElementsResponse",
+]
--- dendrite_python_sdk/models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-
-class CompleteTaskDTO(BaseModel):
-    message: str
-    chat_id: Optional[str] = None
-    stream: Optional[bool] = False
-    store_chat: Optional[bool] = True
