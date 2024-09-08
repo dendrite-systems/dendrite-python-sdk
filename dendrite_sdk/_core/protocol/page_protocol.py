09a6dadfc4895df351ee58996a7ae81cb53f5a8e
Arian Hanifi <counterstrike1337@hotmail.com>
2024-09-08 11:16:36 -0700

    add get_element mixin and update protocol

--- dendrite_sdk/_core/protocol/page_protocol.py
+++ dendrite_sdk/_core/protocol/page_protocol.py
@@ -1,8 +1,9 @@
 from asyncio import Protocol
-from typing import Any
+from typing import Any, List
 
 from dendrite_sdk._api.browser_api_client import BrowserAPIClient
 from dendrite_sdk._core._base_browser import BaseDendriteBrowser
+from dendrite_sdk._core.dendrite_element import DendriteElement
 
 
 class DendritePageProtocol(Protocol):
@@ -15,3 +16,7 @@ class DendritePageProtocol(Protocol):
     browser_api_client: BrowserAPIClient
 
     async def _get_page_information(self) -> Any: ...
+
+    async def get_all_elements_from_selector(
+        self, selector: str
+    ) -> List[DendriteElement]: ...
