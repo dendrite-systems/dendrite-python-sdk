ae0fb66d249acbbcb6be814955fc56bcb725b987
Arian Hanifi <counterstrike1337@hotmail.com>
2024-09-26 13:51:43 +0200

    update test to save download to temp

--- tests/tests_async/test_download.py
+++ tests/tests_async/test_download.py
@@ -15,7 +15,6 @@ async def test_download(dendrite_browser: AsyncDendrite, tmp_path):
     )
     await page.playwright_page.get_by_text("Download File").click()
     dw = await page.get_download(timeout=5000)
-    await dw.save_as(dw.suggested_filename)
 
     # Save the downloaded file to the suggested location
     download_path = tmp_path / dw.suggested_filename
