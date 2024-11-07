85dd343613c28f03f3ddcd98469d3d70b449e3a3
Arian Hanifi <46941672+coffecup25@users.noreply.github.com>
2024-11-07 16:23:37 +0100

    Den 446 Add scroll_to_bottom to dendrite browser (#88)

--- dendrite/__init__.py
+++ dendrite/__init__.py
@@ -18,7 +18,7 @@ logger.remove()
 
 fmt = "<green>{time: HH:mm:ss.SSS}</green> | <level>{level: <8}</level>- <level>{message}</level>"
 
-logger.add(sys.stderr, level="DEBUG", format=fmt)
+logger.add(sys.stderr, level="INFO", format=fmt)
 
 
 __all__ = [
